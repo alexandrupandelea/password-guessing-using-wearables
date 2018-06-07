ccf792c4e2ec8895d8582e0ae41858f05899b1ea
Alexandru Pandelea <alexandru.pandelea@gmail.com>
2018-06-07 15:02:52 +0300

    dataclassifier: add knn classifier
    Add K-nearest-neighbors classifier from the sklearn library. Use
    the feature vector obtained from tsfresh as data.

--- data-processing/dataclassifier.py
+++ data-processing/dataclassifier.py
@@ -10,7 +10,11 @@ from tsfresh.feature_extraction import ComprehensiveFCParameters
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import classification_report
+from sklearn.neighbors import KNeighborsClassifier
 from argparse import ArgumentParser
+
+DECISION_TREE = 'dt'
+KNN = 'knn'
 
 def get_features(y, relevant_features):
     sensor_data_list = dict_as_list(sensor_data)
@@ -35,7 +39,15 @@ def decision_tree_classifier(X, y, test_size):
 
     print(classification_report(y_test, cl.predict(X_test)))
 
-def nr_pressed_keys_decision_tree_classifier(relevant_features, test_size):
+def k_nearest_neighbors_classifier(X, y, test_size):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = test_size)
+
+    cl = KNeighborsClassifier(n_neighbors = 5)
+    cl.fit(X_train, y_train)
+
+    print(classification_report(y_test, cl.predict(X_test)))
+
+def nr_pressed_keys_classifier(relevant_features, test_size, classifier):
     y = {}
 
     for key in pressed_keys.keys():
@@ -61,34 +73,57 @@ def nr_pressed_keys_decision_tree_classifier(relevant_features, test_size):
 
     X = get_features(y, relevant_features)
 
-    decision_tree_classifier(X, y, test_size)
+    if classifier == DECISION_TREE:
+        decision_tree_classifier(X, y, test_size)
+    elif classifier == KNN:
+        k_nearest_neighbors_classifier(X, y, test_size)
+    else:
+        print "please use a valid classifier"
 
-def original_text_decision_tree_classifier(relevant_features):
+def original_text_classifier(relevant_features, test_size, classifier):
     y = {}
 
     for key in pressed_keys.keys():
         text = ""
-        for tupl in pressed_keys[key]:
-            text += tupl[KEY]
+        full_text = ""
+        all_digits = True
 
-        y[key] = text
+        for tupl in pressed_keys[key]:
+            full_text += tupl[KEY]
+
+            if tupl[KEY] in left_hand_keys:
+                text += tupl[KEY]
+            if not tupl[KEY].isdigit():
+                all_digits = False
+
+        # we made the assumption that if the text is
+        # all digits, then it's written only with the left hand
+        if all_digits == True:
+            y[key] = full_text
+        else:
+            y[key] = text
 
     y = pd.Series(y)
 
     X = get_features(y, relevant_features)
 
-    decision_tree_classifier(X, y)
+    if classifier == DECISION_TREE:
+        decision_tree_classifier(X, y, test_size)
+    elif classifier == KNN:
+        k_nearest_neighbors_classifier(X, y, test_size)
+    else:
+        print "please use a valid classifier"
 
 def main():
     p = ArgumentParser()
     p.add_argument('-otc', '--original_text_classifier', dest = 'original_text_classifier',
-        action = 'store', choices = ['dt'],
-        help='Recover original text classifier. Choose classifier type:\n \
-        - dt Decision Tree classifier')
+        action = 'store', choices = [DECISION_TREE, KNN],
+        help='Recover original text classifier. Choose classifier type: \
+        dt Decision Tree classifier; knn K-nearest-neighbor classifier')
     p.add_argument('-knc', '--key_number_classifier', dest = 'key_number_classifier',
-        action = 'store', choices = ['dt'],
-        help='Detect the number of pressed keys .Choose classifier type:\n \
-        - dt Decision Tree classifier')
+        action = 'store', choices = [DECISION_TREE, KNN],
+        help='Detect the number of pressed keys .Choose classifier type: \
+        dt Decision Tree classifier; knn K-nearest-neighbor classifier')
     p.add_argument('-rf', '--relevant_features', action = 'store_true')
     p.add_argument('-ts', '--test_size', type = float, default = 0.2,
         help='The percentage from the dataset to be used for testing')
@@ -98,10 +133,12 @@ def main():
     build_pressed_keys_dict()
     build_sensor_data_dict()
 
-    if args.original_text_classifier and args.original_text_classifier == 'dt':
-        original_text_decision_tree_classifier(args.relevant_features, args.test_size)
-    elif args.key_number_classifier and args.key_number_classifier == 'dt':
-        nr_pressed_keys_decision_tree_classifier(args.relevant_features, args.test_size)
+    if args.original_text_classifier:
+        original_text_classifier(args.relevant_features,
+            args.test_size, args.original_text_classifier)
+    elif args.key_number_classifier:
+        nr_pressed_keys_classifier(args.relevant_features,
+            args.test_size, args.key_number_classifier)
     else:
         print "Please choose a classifier"
 
